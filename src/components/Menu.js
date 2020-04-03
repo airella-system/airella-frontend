f16d77891a2fd3afb4903fbd7328116ee7587e09
Krzysztof PIaskowy <krzychu2956@gmail.com>
2020-04-03 23:38:34 +0200

    Add menu animation

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,24 +1,50 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { FaAngleLeft } from "react-icons/fa";
+import { FaAngleLeft, FaRegTimesCircle, FaRegLightbulb } from "react-icons/fa";
 import '../style/additional/animationLib.scss';
+import '../style/components/menu.scss';
 
 class Menu extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			isOpen: false,
+		};
 	}
 
 	render() {
 		return(
 			// <div className="animated infinite slideInLeft delay-2s">
 			<div className="menuContainter">
-				<div className="hamburger">menu</div>
-				<div className="menuHolder">
-					<div className="close">close</div>
+				<div className="hamburger">
+					<div 
+						className="" 
+						onClick={() => this.setState({ isOpen: true })}
+					>menu</div>
+					</div>
+				<div className={`menuHolder animated ${this.state.isOpen ? "slideInLeft" : "slideOutLeft"}`} >
+					<div className="close">
+						<FaRegTimesCircle 
+							className="closeIcon" 
+							onClick={() => this.setState({ isOpen: false })} 
+						/>
+					</div>
+
 					<Link className="link" to="/test">
 						<div className="holder">
-							<div><FaAngleLeft className="menuIcon" /></div>
+							<div><FaRegLightbulb className="menuIcon" /></div>
+							<div className="textAligner">test</div>
+						</div>
+					</Link>
+					<Link className="link" to="/test">
+						<div className="holder">
+							<div><FaRegLightbulb className="menuIcon" /></div>
+							<div className="textAligner">test</div>
+						</div>
+					</Link>
+					<Link className="link" to="/test">
+						<div className="holder">
+							<div><FaRegLightbulb className="menuIcon" /></div>
 							<div className="textAligner">test</div>
 						</div>
 					</Link>
