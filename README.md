047f5f3f6d7beb3b07a653190dd31bcbc4ba705e
Krzysztof PIaskowy <krzychu2956@gmail.com>
2020-04-03 21:14:20 +0200

    Start working with menu

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 
 ### Usefull links
 - React Icon documentation [here](https://react-icons.netlify.com/#/)
+
+https://daneden.github.io/animate.css/
+https://github.com/daneden/animate.css
+https://react-leaflet.js.org/docs/en/intro
+https://negomi.github.io/react-burger-menu/
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import { FaAngleLeft } from "react-icons/fa";
+import '../style/additional/animationLib.scss';
 
 class Menu extends React.Component {
 	constructor(props) {
@@ -7,7 +10,21 @@ class Menu extends React.Component {
 	}
 
 	render() {
-		return(<div>Menu</div>);
+		return(
+			// <div className="animated infinite slideInLeft delay-2s">
+			<div className="menuContainter">
+				<div className="hamburger">menu</div>
+				<div className="menuHolder">
+					<div className="close">close</div>
+					<Link className="link" to="/test">
+						<div className="holder">
+							<div><FaAngleLeft className="menuIcon" /></div>
+							<div className="textAligner">test</div>
+						</div>
+					</Link>
+				</div>
+			</div>
+		);
 	}
 }
 
