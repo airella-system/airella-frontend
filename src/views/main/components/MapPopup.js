c3bea4db20e97552a1bed4f87cc9c93ab759be8f
Krzysztof PIaskowy <krzychu2956@gmail.com>
2020-04-08 23:59:59 +0200

    Start working with popup stylization

--- src/views/main/components/MapPopup.js
+++ src/views/main/components/MapPopup.js
@@ -5,6 +5,8 @@ import { Popup } from "react-leaflet";
 import { connect } from 'react-redux';
 import { sensorDetailAction } from '../../../redux/actions';
 
+import '../../../style/main/components/MapPopup.scss';
+
 class MapPopup extends Component {
 	constructor(props) {
 		super(props);
@@ -20,23 +22,28 @@ class MapPopup extends Component {
 			<Popup>
 				<div className="popUpContent">
 					<div className="address">{this.props.stationData.city}, {this.props.stationData.address_detail}</div>		
-					<div>
-						Air quality: {this.getAirQualityIcon(this.props.stationData)}
+					<div className="verticalHolder airQuality">
+						<div className="key">Air quality:</div> 
+						<div className="value">{this.getAirQualityIcon(this.props.stationData)}</div>
 					</div>			
 					<div className="verticalHolder">
-						<div className="key">PM 10:</div>
-						<div className="value">{this.props.stationData.pm10}</div>
+						<div className="key">PM 10:
+						<div className="value precentValue">{Math.round(this.props.stationData.pm10 / 45 * 100)}%</div>
+						</div>
+						<div className="value">{this.props.stationData.pm10} <span>µg/m³</span></div>
 					</div>
-					<div className="verticalHolder">
+					<div className="seprator"></div>
+					<div className="verticalHolder ">
 						<div className="key">Temperatura:</div>
-						<div className="value">{this.props.stationData.temperature}</div>
+						<div className="value">{this.props.stationData.temperature} <span>°C</span></div>
 					</div>
+					<div className="seprator"></div>
 					<div className="verticalHolder">
 						<div className="key">Wilgotność:</div>
 						<div className="value">{this.props.stationData.humidity}%</div>
 					</div>
-					<div className="measuremtnTime">{this.props.stationData.last_measurement}</div>
-					<div onClick={() => this.props.dispatch(sensorDetailAction(this.props.stationData))}>szczegóły</div>
+					<div className="measuremtnTime"><span>Ostatni pomiar: </span>{this.props.stationData.last_measurement}</div>
+					<div className="showMoreBtn" onClick={() => this.props.dispatch(sensorDetailAction(this.props.stationData))}>szczegóły</div>
 				</div>
 			</Popup>
 		);
