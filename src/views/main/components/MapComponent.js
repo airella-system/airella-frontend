924d4441352472c11c1f04ae22aff9e7db801084
BluValor <37878700+BluValor@users.noreply.github.com>
2020-08-17 22:39:38 +0200

    rewrite map and popup to react hooks

--- src/views/main/components/MapComponent.js
+++ src/views/main/components/MapComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import {
@@ -11,7 +11,7 @@ import {
   Polygon,
   SVGOverlay,
 } from "react-leaflet";
-import styles from "../../../style/main/components/MapComponent.scss";
+import styles from "../../../style/main/components/MapComponent.module.scss";
 import "leaflet/dist/leaflet.css";
 import { getApiUrl } from "../../../config/ApiURL";
 import AnimatedMapPopup from "./AnimatedMapPopup";
@@ -25,107 +25,62 @@ L.Icon.Default.mergeOptions({
   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  try {
-    let container = document.querySelectorAll(".leaflet-zoom-animated")[1];
-    let defs = L.SVG.create("defs");
-    container.appendChild(defs);
-    defs.insertAdjacentHTML(
-      "beforeend",
-      '<radialGradient id="gradient1" cx="50%" cy="50%" r="50%" fx="50%" fy="50%"> \
-			<stop style="stop-color:#ff4d4d;stop-opacity:1;" offset="0" /> \
-			<stop style="stop-color:#ff4d4d;stop-opacity:0;" offset="1" /> \
-		</radialGradient>'
-    );
-  } catch (e) {
-    console.log(e);
-  }
-});
-
-class MapComponent extends Component {
-  calculateMarkerSize = (zoom) => Math.cos((zoom * Math.PI) / 36) * -10 + 10;
-
-  constructor(props) {
-    super(props);
-
-    this.constans = {
-      lat: 50.0622881,
-      lng: 19.9311482,
-      initialZoom: 13,
-    };
-
-    this.state = {
-      currentMarkerSize: this.calculateMarkerSize(this.constans.initialZoom),
-      stationData: null,
-      isGeolocalizationEnable: false,
-      userCurrentPosition: {
-        lat: 50.0622881,
-        lng: 19.9311482,
-      },
-    };
-  }
-
-  static propTypes = {
-    text: PropTypes.string,
+function MapComponent() {
+  const constans = {
+    lat: 50.0622881,
+    lng: 19.9311482,
+    initialZoom: 13,
   };
 
-  componentDidMount() {
-    this.trySetMapPositionWithGeolocation();
-    this.updateMarkers();
-  }
+  const calculateMarkerSize = (zoom) => Math.cos((zoom * Math.PI) / 36) * -10 + 10;
 
-  setMapPositionWithGeolocation() {
+  const [currentMarkerSize, setCurrentMarkerSize] = useState(calculateMarkerSize(constans.initialZoom))
+
+  const [stationData, setStationData] = useState(null)
+  
+  const [stationFullData, setStationFullData] = useState({})
+
+  const [stationDataLastFetchDate, setStationDataLastFetchDate] = useState(null)
+
+  const [isGeolocalizationEnable, setIsGeolocalizationEnable] = useState(false)
+
+  const [userCurrentPosition, setUserCurrentPosition] = useState({
+    lat: 50.0622881,
+    lng: 19.9311482,
+  })
+
+  const [leafletMap, setLeafletMap] = useState(null)
+
+  const setMapPositionWithGeolocation = () => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        this.setState({
+        setIsGeolocalizationEnable(true)
+        setUserCurrentPosition({
           lat: position.coords.latitude,
           lng: position.coords.longitude,
-          isGeolocalizationEnable: true,
-          userCurrentPosition: {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          },
-        });
+        })
       },
-      (error) =>
-        console.error("Error Code = " + error.code + " - " + error.message)
+      (error) => console.error("Error Code = " + error.code + " - " + error.message)
     );
   }
 
-  trySetMapPositionWithGeolocation() {
+  const trySetMapPositionWithGeolocation = () => {
     if (!("geolocation" in navigator)) {
       return false;
     }
     if (navigator.permissions) {
       navigator.permissions.query({ name: "geolocation" }).then((info) => {
         if (info.state == "granted") {
-          this.setMapPositionWithGeolocation();
+          setMapPositionWithGeolocation();
         }
       });
     } else {
       // Safari doesn't support navigator.permissions
-      this.setMapPositionWithGeolocation();
+      setMapPositionWithGeolocation();
     }
   }
 
-  getStationData(stationId) {
-    let key = `station${stationId}Key`;
-    fetch(
-      getApiUrl("getPopupData", [stationId], {
-        strategy: "latest",
-      })
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.data);
-        this.setState({
-          [key]: data.data,
-        });
-      })
-      .catch((e) => console.error(e));
-  }
-
-  getMarkers(lat, lng, radius) {
+  const getMarkers = (lat, lng, radius) => {
     fetch(
       getApiUrl("getMarkers", null, {
         latitude: lat,
@@ -136,19 +91,45 @@ class MapComponent extends Component {
       .then((response) => response.json())
       .then((data) => {
         let currentDate = Date();
-        if (!this.state.stationDataFetchDate || currentDate > this.state.stationDataLastFetchDate) {
-          this.setState({
-            stationData: data.data,
-            stationDataLastFetchDate: currentDate
-          });
+        if (!stationDataLastFetchDate || currentDate > stationDataLastFetchDate) {
+          setStationDataLastFetchDate(currentDate)
+          console.log(data.data)
+          setStationData(data.data)
         }
       })
       .catch((e) => console.error(e));
   }
 
-  renderMarkers(map) {
-    if (!this.state.stationData) return;
-    return this.state.stationData.map((item, index) => {
+  const updateMarkers = () => {
+    if (!leafletMap) return;
+
+    let bounds = leafletMap.leafletElement.getBounds();
+    let center = leafletMap.leafletElement.getCenter();
+    let radius = leafletMap.leafletElement.distance(bounds._northEast, center);
+
+    // this.getMarkers(center.lat, center.lng, radius); // todo markers logic
+    getMarkers(center.lat, center.lng, 1000000);
+  }
+
+  const getStationData = (stationId) => {
+    let key = `station${stationId}Key`;
+    fetch(
+      getApiUrl("getPopupData", [stationId], {
+        strategy: "latest",
+      })
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.data);
+        setStationFullData({...stationFullData, [key]: data.data})
+      })
+      .catch((e) => console.error(e));
+  }
+
+  const renderMarkers = () => {
+    if (!stationData) return;
+    
+    return stationData.map((item, index) => {
       let position = {
         lat: item.location.latitude,
         lng: item.location.longitude,
@@ -156,87 +137,70 @@ class MapComponent extends Component {
       let stationId = item.id;
       let stationDataKey = `station${stationId}Key`;
       let color = AirQualityColors[indexToLevel(item.aqi)];
-      let hiddenClass = "";
+      
       return (
         <CircleMarker
-          className={hiddenClass}
           key={index}
           center={position}
           fillColor={color}
           color={color}
-          onClick={() => this.getStationData(stationId)}
+          onClick={() => {
+            getStationData(stationId)
+          }}
           fillOpacity={0.3}
           opacity={1}
-          radius={this.state.currentMarkerSize}
+          radius={currentMarkerSize}
         >
           <AnimatedMapPopup
-            stationData={this.state[stationDataKey]}
+            stationData={stationFullData[stationDataKey]}
             color={color}
-            onOpen={() => (hiddenClass = styles.hiddenMarker)}
           />
         </CircleMarker>
       );
     });
   }
 
-  currentPositionMarker() {
-    if (!this.state.isGeolocalizationEnable) return;
+  const currentPositionMarker = () => {
+    if (!isGeolocalizationEnable) return;
     return (
       <CircleMarker
-        center={this.state.userCurrentPosition}
+        center={userCurrentPosition}
         fillColor="#5078de"
         color="#5078de"
         onClick={() => {}}
-      ></CircleMarker>
+      />
     );
   }
 
-  updateMarkers = () => {
-    let bounds = this.leafletMap.leafletElement.getBounds();
-    let center = this.leafletMap.leafletElement.getCenter();
-    let radius = this.leafletMap.leafletElement.distance(bounds._northEast, center);
+  useEffect(() => {
+    trySetMapPositionWithGeolocation();
+    updateMarkers();
+  }, [leafletMap])
 
-    // this.getMarkers(center.lat, center.lng, radius); // todo markers logic
-    this.getMarkers(center.lat, center.lng, 1000000);
-  }
-
-  render() {
-    const position = [this.constans.lat, this.constans.lng];
-
-    return (
-      <div className="mapContainer">
-        <Map
-          zoomControl={false}
-          center={position}
-          zoom={this.constans.initialZoom}
-          ref={(m) => {
-            this.leafletMap = m;
-          }}
-          onzoomend={(x) => {
-            this.setState({
-              currentMarkerSize: this.calculateMarkerSize(
-                this.leafletMap.leafletElement.getZoom()
-              ),
-            })
-          }
-          }
-          // onMoveEnd={
-          //   (x) => this.updateMarkers()
-          // }
-          // todo markers logic
-          className="map"
-        >
-          <TileLayer
-            attribution='<a href="//basemaps.cartocdn.com">Basemap</a> | &copy; <a href="//osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-          />
-          {this.renderMarkers(this.leafletMap)}
-          {this.currentPositionMarker()}
-        </Map>
-        <div className="airellaLogo">Airella</div>
-      </div>
-    );
-  }
+  return (
+    <div className={styles.container}>
+      <Map
+        zoomControl={false}
+        center={[constans.lat, constans.lng]}
+        zoom={constans.initialZoom}
+        ref={(m) => setLeafletMap(m)}
+        onzoomend={(x) => setCurrentMarkerSize(calculateMarkerSize(leafletMap.leafletElement.getZoom()))}
+        // onMoveEnd={
+        //   (x) => this.updateMarkers()
+        // }
+        // todo markers logic
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='<a href="//basemaps.cartocdn.com">Basemap</a> | &copy; <a href="//osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+        />
+        {renderMarkers()}
+        {currentPositionMarker()}
+      </Map>
+      <div className={styles.logo}>Airella</div>
+    </div>
+  );
 }
 
 function mapStateToProps(state) {
