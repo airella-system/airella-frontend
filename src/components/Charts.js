b014176a74f6b6c596d964a2d8831061034ce1f0
BluValor <37878700+BluValor@users.noreply.github.com>
2020-10-08 01:07:40 +0200

    initial charts layout and options menu

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -6,12 +6,61 @@ import Button from "./Button";
 import Popup from "./Popup";
 import { FaTimes } from "react-icons/fa";
 import { setSensorChart } from "../redux/actions";
-import { login as loginFunction } from "../config/ApiCalls";
+import ChartTabs from "../views/main/components/ChartTabs";
+
+function ChartsMenu(props) {
+  const [showData, setShowData] = useState({})
+
+  const handleCheckboxChange = event => {
+    setShowData({
+      ...showData,
+      [event.target.name]: event.target.checked,
+    })
+  }
+
+  useEffect(() => {
+    if (!props.sensorData)
+      return
+    var initialShowData = {}
+    for (const data of props.sensorData.sensors)
+      initialShowData = {
+        ...initialShowData,
+        [data.id]: false,
+      }
+    setShowData(initialShowData)
+  }, [props.sensorData])
+
+  return (
+    <div className={styles.menu}>  
+      <div className={styles.checkboxContainer}>
+        {Object.entries(showData).map(([name, value]) => {
+          return (
+            <label className={styles.container} key={name}>
+              {name}
+              <input name={name} type="checkbox" checked={value} onChange={handleCheckboxChange}/>
+              <span className={styles.checkmark}></span>
+            </label>
+            )
+        })}
+      </div>
+      <Button onClick={() => props.onApplyClick && props.onApplyClick(showData)}>
+        APPLY
+      </Button>
+    </div>
+  )
+}
 
 function Charts(props) {
+  
+  const [showData, setShowData] = useState({})
 
   const close = () => {
     props.dispatch(setSensorChart(false))
+  }
+
+  const onApplyClick = values => {
+    console.log("apply")
+    console.log(values)
   }
 
   return (
@@ -23,6 +72,15 @@ function Charts(props) {
               <Button onClick={() => close()}>
                 <FaTimes size={22} rotate={45} />
               </Button>
+            </div>
+            <ChartsMenu sensorData={props.sensorData} onApplyClick={onApplyClick}/>
+            <div className={styles.chartContainer}>
+              {props.sensorData != null && (
+                <ChartTabs
+                  key={props.sensorData.id}
+                  stationId={props.sensorData.id}
+                />
+              )}
             </div>
           </div>
         ) : (
@@ -36,6 +94,7 @@ function Charts(props) {
 function mapStateToProps(state) {
   return {
     visibility: state.sensorChart.visibility,
+    sensorData: state.sensorDetail.sensorData,
   };
 }
 
